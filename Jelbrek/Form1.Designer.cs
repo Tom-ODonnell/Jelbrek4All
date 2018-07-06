d3e2ae50d37ee8b2f17d4dfbd8a0af148bdcc680
Tom-ODonnell <tomodonnell000@gmail.com>
2018-07-05 21:20:47 -0400

    add basic UI... functionality eta son

---
+++ Jelbrek/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Jelbrek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe Script", 32F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label1.Location = new System.Drawing.Point(18, -3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(215, 243);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "J͍̘̳̻̼͈͓̗̬͓͍͇̹̖̤͌̐ͭ͋ͫ̋ͩ̾͐̊ͥͭe͍̤͓̯̦͓̖̺̳̗̼̦̬͌̃̌̈́ͮͬ́͐l̺͈͚̜̜͖͈͈̬̬͊ͨ͐̈͗ͬ̃͊ͮb̂ͨ̐̽̎̾ͣ̚̚" +
+    "̖̦̘̳̭̲̅ͧ̓͆̒̈́̈͊r̘̱̪̯͓̠͓̠ͧͣ́̌ͯ̀ͩ͐̋̐ͨͮe̤̫̺͑̾̎ͣͧk̖͙̱̰̩̮͓̤͓̦̘̗̠͉̬̩͔̄͒͒̇ͩ̀ͭ͑";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Segoe Script", 22F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label2.Location = new System.Drawing.Point(219, -3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(289, 50);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Jelbrek 4 all iSO";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Segoe Script", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(239, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(203, 31);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "By Tom O\'Donnell";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Sitka Heading", 53.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.button1.Location = new System.Drawing.Point(185, 313);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(384, 116);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Jelbrek Me";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(245, 105);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(434, 202);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "Console 4 jelbrek...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Jelbrek.Properties.Resources.better;
+            this.ClientSize = new System.Drawing.Size(734, 441);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = resources.GetString("$this.Text");
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
