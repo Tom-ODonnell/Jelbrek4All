8cfd7824c2732c432447856886baf206da7c7b3e
Tom-ODonnell <tomodonnell000@gmail.com>
2018-07-06 11:50:03 -0400

    update UI and add installation check

--- Jelbrek/Form1.cs
+++ Jelbrek/Form1.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +17,31 @@ namespace Jelbrek
         public Form1()
         {
             InitializeComponent();
+            if (!Directory.Exists("c:/Jelbrek4All"))
+            {
+                if (DialogResult.OK== MessageBox.Show("Jelbrek4All was not installed correctly... please reinstall using the isntaller.", "Oops!", MessageBoxButtons.OK, MessageBoxIcon.Error))
+                {
+                    System.Environment.Exit(1);
+                }
+            }
+        }
+
+        public static string ExecuteCommand(string command)
+        {
+            ProcessStartInfo processInfo;
+            Process process;
+
+            processInfo = new ProcessStartInfo("cmd.exe", "/c " + command);
+            processInfo.CreateNoWindow = true;
+            processInfo.UseShellExecute = false;
+            processInfo.RedirectStandardOutput = true;
+
+            process = Process.Start(processInfo);
+            process.WaitForExit();
+            string output = process.StandardOutput.ReadToEnd();
+
+            process.Close();
+            return output.ToString();
         }
     }
 }
